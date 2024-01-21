eb92e62a7909e447291e72b8ffde549f05d847d0
Andriy Kupchanko <80577186+andriykupchanko@users.noreply.github.com>
2024-01-21 22:01:05 +0200

    create first app

--- sss/ViewController.swift
+++ sss/ViewController.swift
@@ -13,7 +13,12 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func showAlert() {
+        let alert = UIAlertController(title: "Hey guyce", message: "Its my first alert in swift", preferredStyle:.alert)
+        let action = UIAlertAction(title: "Awesome!", style: .default)
+        alert.addAction(action)
+        present(alert,animated: false)
+   }
 
 }
 
