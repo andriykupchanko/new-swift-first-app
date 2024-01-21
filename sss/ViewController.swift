99891524d6b870da1d344e911a346bc399b0f137
Andriy Kupchanko <80577186+andriykupchanko@users.noreply.github.com>
2024-01-21 21:33:36 +0200

    Initial Commit

---
+++ sss/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sss
+//
+//  Created by Andrew Kupchanko on 21.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
